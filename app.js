42b76c54445f4c61faf78312858d358e103e864d
Victor Nascimento <victormota15@gmail.com>
2017-03-16 11:12:53 -0300

    Test arc

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-matrix.led('red').render();
+matrix.led({
+  // degrees of arc [ 90° = quadrant ]   
+  arc: 160,
+  color: 'yellow',
+  // index to start drawing arc
+  start: 1
+});
+
+matrix.led({
+  // degrees of arc [ 90° = quadrant ]   
+  arc: 160,
+  color: 'red',
+  // index to start drawing arc
+  start: 18
+});
+
 
 matrix.service('demographics').start().then(function(data){
   matrix.led('green').render();
