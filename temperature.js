ed9fe5004a67c540082191546d31c23fbf6adedd
jkdrangel <jkdrangel@hotmail.com>
2017-03-16 11:13:21 -0300

    humidity and temperature

---
+++ temperature.js
@@ -0,0 +1,20 @@
+var options = {
+  refresh: 1000, //milliseconds
+  timeout: 1000 //milliseconds
+};
+matrix.init('temperature', options).then(function(data){
+	if(data.value>=28){
+		matrix.led({
+			 arc: 180,
+			color: 'red',
+			start: 1
+		}).render();
+	}else if(data.value>=24){
+		matrix.led({
+			 arc: 180,
+			color: 'orange',
+			start: 1
+		}).render();
+	} 
+});
+
